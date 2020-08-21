abe4e1ddb824d1dca9fd6a606701353700dec734
Saran Kumar Allam <44534303+saran1209@users.noreply.github.com>
2020-08-21 05:14:22 -0400

    Add files via upload

---
+++ JavaScript Calculator/logic.js
@@ -0,0 +1,59 @@
+let screen = document.getElementById('screen');
+
+let buttons = document.querySelectorAll('button');
+let screenValue = "";
+let clear ="";
+for(item of buttons){
+    item.addEventListener('click', (e)=>{
+        buttonText = e.target.innerText;
+        console.log(buttonText);
+        if(buttonText== 'X'){
+            buttonText ='*';
+            screenValue += buttonText;
+            screen.value = screenValue ;
+        
+        }
+        else if(buttonText=='sin') {
+            screenValue = Math.sin(screenValue);
+            screen.value = screenValue;
+
+            
+        }
+        else if(buttonText=='cos') {
+            screenValue = Math.cos(screenValue);
+            screen.value = screenValue;
+
+            
+        }
+        else if(buttonText=='tan') {
+            screenValue = Math.tan(screenValue);
+            screen.value = screenValue;
+
+            
+        }
+        
+        else if(buttonText== 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+        
+        
+        
+        }
+        else if(buttonText== '='){
+            screenValue = eval(screenValue);
+            screen.value = screenValue;
+        }
+        else if(buttonText== '.'){
+            screenValue += buttonText;
+            screen.value = screenValue ;
+        }
+        else if(buttonText=='AC'){
+            screenValue = screenValue.substring(0, screenValue.length - 1);
+            screen.value = screenValue;
+        }
+        else {
+           
+            screenValue += buttonText;
+            screen.value = screenValue ;
+        }
+    })};
